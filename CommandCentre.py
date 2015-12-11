c5e61ecb22645874ea462abce1f234f97fc22635
Charlie Cook <charles.cook@digital.hmrc.gov.uk>
2015-12-11 13:49:36 +0000

    First test of websocket connection.

---
+++ CommandCentre.py
@@ -0,0 +1,16 @@
+import json
+import websocket
+
+from APIToken import token
+
+stream_url = "wss://stream.pushbullet.com/websocket/" + token
+
+
+def received_message(ws, message):
+    parsed_json = json.load(message)
+    if parsed_json['subtype'] == 'push':
+        print("Received a push")
+
+pushbullet_stream = websocket.WebSocketApp(stream_url, on_message=received_message)
+
+pushbullet_stream.run_forever()
