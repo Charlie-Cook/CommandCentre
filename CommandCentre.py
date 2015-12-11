9a776b5f9404ec9c21119158cd6dce5e6c6b6d05
Charlie Cook <charles.cook@digital.hmrc.gov.uk>
2015-12-11 14:31:35 +0000

    Retrieving pushes is now working.

--- CommandCentre.py
+++ CommandCentre.py
@@ -1,16 +1,28 @@
 import json
+import requests
 import websocket
+from sys import exit
 
 from APIToken import token
 
 stream_url = "wss://stream.pushbullet.com/websocket/" + token
+push_url = "https://api.pushbullet.com/v2/pushes?limit=1"
+header = {'Access-Token': token}
+run = True
 
+pushbullet_stream = websocket.create_connection(stream_url)
 
-def received_message(ws, message):
-    parsed_json = json.loads(message)
-    if parsed_json['subtype'] == 'push':
-        print("Received a push")
-
-pushbullet_stream = websocket.WebSocketApp(stream_url, on_message=received_message)
-
-pushbullet_stream.run_forever()
+while run is True:
+    result = pushbullet_stream.recv()
+    try:
+        parsed_json = json.loads(result)
+        if parsed_json['subtype'] == 'push':
+            latest_push = requests.get(push_url, headers=header)
+            if latest_push.status_code == 200:
+                push_json = json.loads(latest_push.text)
+                if push_json['pushes'][0]['type'] == 'note':
+                    print(push_json['pushes'][0]['body'])
+                    if push_json['pushes'][0]['body'] == 'Exit':
+                        exit
+    except KeyError:
+        pass
