0bd97d6aaf415a969a2397b35854f3862e51b0ec
Charlie Cook <charles.cook@digital.hmrc.gov.uk>
2016-03-03 14:38:51 +0000

    Added Python project files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CommandCentre',
+    version='v1.0',
+    packages=['', 'plugins'],
+    url='',
+    license='Apache License, Version 2.0',
+    author='Charlie Cook',
+    author_email='charliedcook@gmail.com',
+    description='Remote administration script featuring a plugin system.'
+)
