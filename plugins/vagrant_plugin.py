d616c09508f30cb26a9f554e46c32ed88f84bf78
Charlie Cook <charles.cook@digital.hmrc.gov.uk>
2016-05-19 14:01:16 +0100

    Change to path in Vagrant script

--- plugins/vagrant_plugin.py
+++ plugins/vagrant_plugin.py
@@ -6,9 +6,11 @@ hook = 'Vagrant'
 def run_command(command):
     if command == "up":
         p = subprocess.Popen(['powershell.exe',
-                              'C:\\Users\\Digital4357\\Documents\\PowershellScripts\\VagrantCommands -command up'])
+                              'C:\\Users\\MyUser\\Documents\\PowershellScripts\\VagrantCommands -command up'])
         p.communicate()
     elif command == "halt":
         p = subprocess.Popen(['powershell.exe',
-                              'C:\\Users\\Digital4357\\Documents\\PowershellScripts\\VagrantCommands -command halt'])
+                              'C:\\Users\\MyUser\\Documents\\PowershellScripts\\VagrantCommands -command halt'])
         p.communicate()
+
+# Create powershell script to start or stop vagrant then update the above path
